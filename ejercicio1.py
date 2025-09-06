91d1c70d2a1da7811e1125d80814f3b03511351f
Gerardo Santiago Vazquez Tovar <neowhatup@gmail.com>
2025-09-05 18:07:00 -0600

    viernes

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,14 @@
 
+# suma de dos numeros
+num1 = int(input( "Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
+suma = num1 + num2
+print(f"La suma es: {suma}")
+
+
+# Numero par o impar
+num = int(input("Ingresa un numero: "))
+if num % 2 == 0:
+    print(f"El numero: {num} es par")
+else:
+    print(f"El numero: {num} es impar")
